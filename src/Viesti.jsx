244c42e24bfa92992ca9c85a896e3836f7ddf21d
Mika Koivisto <mika.koivisto@student.careeria.fi>
2022-05-14 16:09:43 +0300

    Ensimmäinen commit

---
+++ src/Viesti.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+const Viesti = (props) => (
+    <>
+        <p>{props.teksti}</p>
+    </>
+  );
+
+export default Viesti;
