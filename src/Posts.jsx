9be359be3fc8504245a7ed3c899adb3280da449d
Mika Koivisto <mika.koivisto@student.careeria.fi>
2022-05-15 16:52:24 +0300

    Kotitehtävien ratkaisumallit

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -6,6 +6,7 @@ const Posts = () => {
 
 //Komponentin tilan määritys
 const [posts, setPosts] = useState([])
+const [showPosts, setShowPosts] = useState(false)
 
 useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -17,11 +18,12 @@ useEffect(() => {
 
   return (
     <>
-        <h2>Posts from typicode</h2>
+        <h2 onClick={() => setShowPosts(!showPosts)}>Posts from typicode</h2>
 
         {
-            posts && posts.map(p =>
+            showPosts && posts && posts.map(p =>
             <div className='posts' key={p.id}>
+            <h1>User ID: {p.userId}</h1>
             <h2>{p.title}</h2>
             <p>{p.body}</p>
             </div>
